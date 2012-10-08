cb250d7036d160f5fc74e0cb5257b3adc2ec3f42
Karl Fischmann <kfischmann@gmail.com>
2012-10-08 18:08:27 +0200

    Initial DJT commit

---
+++ polls/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from polls.models import Choice, Poll
+
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+
+def detail(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+	selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+	# redisplay the poll voting form.
+	return render_to_response('polls/detail.html', {
+	    'poll': p,
+	    'error_message': "You didn't select a choice.",
+	}, context_instance=RequestContext(request))
+    else:
+	selected_choice.votes += 1
+	selected_choice.save()
+	return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+
+def results(request, poll_id):
+    poll_list = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll_list': poll_list})
